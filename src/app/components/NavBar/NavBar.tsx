53e2f1fcc08722dc679831045a81ac9baf4c6fbf
ek90 <erkan.kaban@cogniss.com>
2024-11-29 14:31:48 +1100

    feat: made some css changes, animation of writing dissapearing, borders etc etc

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -5,15 +5,15 @@ const NavBar = () => {
     return (
         <header className={styles.navbar}>
             <div className={styles.navBarContainer}>
-                <h1>Mindventure <br></br> Hikes</h1>
+            <h1 className={styles.titleAnimation}>Welcome to <br></br></h1>
+                <h1 className={styles.navTitle}>Mindventure <br></br> Hikes</h1>
                 <nav className={styles.navLinks}>
                     <a href="#home">About Us</a>
                     <a href="#about">Charity Hikes</a>
-                    <a href="#services">Colabrate With Us</a>
+                    <a href="#services">Collaborate With Us</a>
                     <a href="#contact">International Tours</a>
                 </nav>
             </div>
-
         </header>
     )
 }
