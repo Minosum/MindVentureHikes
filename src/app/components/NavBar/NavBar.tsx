c2f796d8f1237a492eb8dc8d9d6bb52cec1f1d9f
ek90 <erkan.kaban@cogniss.com>
2024-11-29 20:14:14 +1100

    feat: burger menu added

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <header className={styles.navbar}>
+        <header className={`${styles.navbar} ${isMenuOpen ? styles.menuOpen : ''}`}>
             <div className={styles.navBarContainer}>
-            <h1 className={styles.titleAnimation}>Welcome to <br></br></h1>
+                <h1 className={styles.titleAnimation}>Welcome to <br></br></h1>
                 <h1 className={styles.navTitle}>Mindventure <br></br> Hikes</h1>
-                <nav className={styles.navLinks}>
-                    <a href="#home">About Us</a>
-                    <a href="#about">Charity Hikes</a>
-                    <a href="#services">Collaborate With Us</a>
-                    <a href="#contact">International Tours</a>
+                
+
+                {/* Burger Menu Button */}
+                
+                    <div
+                        className={`${styles.burgerMenu} ${isMenuOpen ? styles.open : ''}`}
+                        onClick={toggleMenu}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+               
+
+
+                <nav className={`${styles.navLinks} ${isMenuOpen ? styles.mobileMenuOpen : ''}`}>
+                    <a href="#home" onClick={toggleMenu}>About Us</a>
+                    <a href="#about" onClick={toggleMenu}>Charity Hikes</a>
+                    <a href="#services" onClick={toggleMenu}>Collaborate With Us</a>
+                    <a href="#contact" onClick={toggleMenu}>International Tours</a>
                 </nav>
             </div>
         </header>
