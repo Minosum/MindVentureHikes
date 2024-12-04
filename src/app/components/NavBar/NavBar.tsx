c30c4a3e06f8cab17c0d3f4c91a7727acdf687bb
ek90 <erkan.kaban@cogniss.com>
2024-12-04 11:54:18 +1100

    fix: css tweaks

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     return (
         <header className={styles.navBar}>
             <div className={styles.navBarContainer}>
-                <h1 className={styles.titleAnimation}>WELCOME TO<br></br></h1>
+                {/* <h1 className={styles.titleAnimation}>WELCOME TO<br></br></h1> */}
                 <h1 className={styles.navTitle}>Mindventure <br></br> Hikes</h1>
                 <div className={styles.navMessageContainer}>
                     <p className={styles.navMessage}>MindVenture is where adventure meets connection. Through hiking, wellness and charity initiatives, we inspire people to explore nature, connect with others, and give back to communities in need. Together, let’s make a difference and create unforgettable journeys.</p>
