80f072f0061f4d62d433ba35ca86220b9ab28426
ek90 <erkan.kaban@cogniss.com>
2025-04-16 18:51:53 +1000

    FIX: changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
-import type { NextConfig } from "next";
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-    output: 'export',  // Static HTML export
-    basePath: '/MindVentureHikes', // Your repo name
-    assetPrefix: '/MindVentureHikes/', // Prefix for assets
-    images: {
-      unoptimized: true, // Required for static export
-    },
-    trailingSlash: true,
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? '/MindVentureHikes' : '',
+  assetPrefix: isGithubPages ? '/MindVentureHikes/' : '',
+  trailingSlash: true,
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
--- src/app/our-story/page.tsx
+++ src/app/our-story/page.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import NavBar from '../components/NavBar/NavBar'
 import { pageData } from '@/app/data/pageData'
 
+
 const page = () => {
     return (
-        <div style={{ height: '700px' }}>
+        <div style={{ height: 'auto' }}>
             <NavBar heading={pageData.story.heading} description={pageData.story.content} />
         </div>
     )
