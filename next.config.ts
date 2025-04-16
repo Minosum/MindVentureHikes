803eedc6da6e31c5ff63e22ff836bcabc49115a9
ek90 <erkan.kaban@cogniss.com>
2025-04-16 17:50:25 +1000

    fix: changes

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,13 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/MindVentureHikes" : "",
-  assetPrefix: isProd ? "/MindVentureHikes/" : "",
-  trailingSlash: true,
+    output: 'export',  // Static HTML export
+    basePath: '/MindVentureHikes', // Your repo name
+    assetPrefix: '/MindVentureHikes/', // Prefix for assets
+    images: {
+      unoptimized: true, // Required for static export
+    },
+    trailingSlash: true,
 };
 
 export default nextConfig;
